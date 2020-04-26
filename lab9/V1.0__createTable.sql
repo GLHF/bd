5406cffdb5b448d99468cbe2775de10eac0cc29b
Timm <timur.galiakberov@gost-group.com>
2020-04-26 16:15:47 +0300

    lab9

--- lab9/V1.0__createTable.sql
+++ lab9/V1.0__createTable.sql
@@ -4,13 +4,22 @@ CREATE TABLE table_name (
     c int
 );
 
-CREATE INDEX a_index ON table_name USING btree(a);
-CREATE INDEX a_b_index ON table_name USING btree(a,b);
-CREATE INDEX a_c_index ON table_name USING btree(a,c);
-CREATE INDEX b_index ON table_name USING btree(b);
-CREATE INDEX b_c_index ON table_name USING btree(b, c);
-CREATE INDEX c_index ON table_name USING btree(c);
-CREATE INDEX a_b_c_index ON table_name USING btree(a, b, c);
+
+CREATE INDEX a_index ON table_name USING btree (a);
+CREATE INDEX c_index ON table_name USING btree (c);
+CREATE INDEX b_index ON table_name USING btree (b);
+CREATE INDEX a_b_index ON table_name USING btree (a, b);
+CREATE INDEX b_a_index ON table_name USING btree (b, a);
+CREATE INDEX a_c_index ON table_name USING btree (a, c);
+CREATE INDEX c_a_index ON table_name USING btree (c, a);
+CREATE INDEX c_b_index ON table_name USING btree (c, b);
+CREATE INDEX b_c_index ON table_name USING btree (b, c);
+CREATE INDEX a_b_c_index ON table_name USING btree (a, b, c);
+CREATE INDEX a_c_b_index ON table_name USING btree (a, c, b);
+CREATE INDEX b_a_c_index ON table_name USING btree (b, a, c);
+CREATE INDEX b_c_a_index ON table_name USING btree (b, c, a);
+CREATE INDEX c_b_a_index ON table_name USING btree (c, b, a);
+CREATE INDEX c_a_b_index ON table_name USING btree (c, a, b);
 
 
 insert into table_name(a,b,c)
