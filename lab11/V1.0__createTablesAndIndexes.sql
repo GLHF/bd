fb91ccfe6d7b473730da82b87913d447ebb3211c
Timm <timur.galiakberov@gost-group.com>
2020-04-26 15:41:14 +0300

    lab11

--- lab11/V1.0__createTablesAndIndexes.sql
+++ lab11/V1.0__createTablesAndIndexes.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE hash_table
 (
     id   integer,
@@ -20,17 +19,17 @@ CREATE TABLE brin_table
 
 CREATE INDEX hash_index ON hash_table USING hash(name);
 CREATE INDEX btree_index ON btree_table USING btree(name);
-CREATE INDEX brin_table ON brin_table USING brin(name);
+CREATE INDEX brin_index ON brin_table USING brin(name) WITH (pages_per_range=4, autosummarize=true);
 
 
 insert into hash_table
-SELECT generate_series(1,100000) as a,
-       round(random() * 999999);
+SELECT generate_series(1,100000),
+       generate_series(1,100000);
 
 insert into btree_table
-SELECT generate_series(1,100000) as a,
-       round(random() * 999999);
+SELECT generate_series(1,100000),
+       generate_series(1,100000);
 
 insert into brin_table
-SELECT generate_series(1,100000) as a,
-       round(random() * 999999);
+SELECT generate_series(1,100000),
+       generate_series(1,100000) ;
